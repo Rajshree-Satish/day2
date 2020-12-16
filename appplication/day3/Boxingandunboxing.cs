8c0a2e9e2e8df710290e157ec254226b94c5a703
rajshree pathak <Rajshree.Satish@lntinfotech.com>
2020-12-16 10:53:54 +0530

    new

---
+++ appplication/day3/Boxingandunboxing.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day3
+{
+    class Boxingandunboxing
+    {
+        static void ss(object s)
+        {
+            Console.WriteLine(s);
+        }
+        public static void Main(string[] args)
+        {
+            int x = 789;
+            object ox = x;
+            Console.WriteLine($" x int value is {x}\n ox object value is {ox}");
+            object ob1 = 45;
+            ss(ob1);
+            int a = (int)ob1;
+            Console.WriteLine($"ob1 value is {ob1} \n a int value is { a} ");
+
+        }
+    }
+}
---
+++ appplication/day3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day3
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public string Location { get; set; }
+        public Employee getemployeedeatils(int id,string name,string desig,string loc)
+        {
+            Employee emp = new Employee();
+            emp.Id = id;
+            emp.Name = name;
+            emp.Designation = desig;
+            emp.Location = loc;
+            return emp;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            Employee emp1 = employee.getemployeedeatils(1, "peter", "mech engn", "pune");
+            Console.WriteLine("employee details\n----------");
+            Console.WriteLine($"id={ emp1.Id} \n name = {emp1.Name}\n designation = {emp1.Designation}\n Location ={emp1.Location}");
+            Console.ReadLine();
+        }
+    }
+}
